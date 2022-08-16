5d52ae0a6da9352e8701ff224b8361611ceaf756
abysswarrior <mehran.safaripour@gmail.com>
2022-08-15 23:12:22 -0700

    fix heroku pipeline error

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,3 +140,4 @@ EMAIL_HOST_USER = "mehran.safaripour@gmail.com"
 EMAIL_HOST_PASSWORD = "sogmdtpuizeryrhq"
 DEFAULT_FROM_EMAIL = "Promethe Social"  # default sign in every email
 
+django_heroku.settings(locals())
